afa3217cf4ea4d86657b4ad771106408dbefb4dc
Balamurali Pandranki <balamurali@live.com>
2015-08-11 15:23:21 +0530

    removed old route files and standard response for get '/'

--- routes.js
+++ routes.js
@@ -4,8 +4,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-      res.json({ message: 'this is end of your limits dude!!!', 
-                 uptime: process.uptime() });   
+      res.json({ code:200,
+                 message: 'this is end of your limits dude!!!', 
+                 uptime: process.uptime()
+               });   
 });
 
 module.exports = router;
