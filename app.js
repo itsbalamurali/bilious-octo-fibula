53464d060feba6bbfe758d1439b94ec03d0239ba
Balamurali Pandranki <balamurali@live.com>
2015-08-11 16:01:14 +0530

    removed views engine totally

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ var routes = require('./routes');
 
 var app = express();
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -26,9 +22,9 @@ app.use('/api', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.status = 404;
+  res.send({ code:404,message: 'Not found'});
+  next(res);
 });
 
 // error handlers
