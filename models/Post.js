655a01dbb0ba243595e96ac29bb323c174f459d3
Balamurali Pandranki <balamurali@live.com>
2015-08-17 16:13:45 +0530

    few modifications to Post model

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,17 @@
 var mongoose = require('mongoose');
 var timestamps = require('mongoose-timestamp');
+var likes = require('mongoose-voting');
 
 var postSchema = new mongoose.Schema({
+	  message:{ type: String, required:true},
+	  author: { type: mongoose.Schema.ObjectId, ref: 'User' },
+	  type: { type: String, required:true},//image_upload,video_upload,file_share,update_status
+	  images:[{ type:String }],//array of image urls
+	  video:{ type: String}, //video url
+	  files: [{ type: String }],	
+	  location: { type:[Number], index:'2d'},
 });
 
 postSchema.plugin(timestamps);
+postSchema.plugin(likes);
 module.exports = mongoose.model('Post', postSchema);
