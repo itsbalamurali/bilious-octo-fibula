876c344088980fc0575c19c2da2d84ab552e7672
Gouthamve <cs14btech11014@iith.ac.in>
2015-08-18 22:11:42 +0530

    Cleanup Anon post Create
    Made message compulsory.

--- controllers/AnonymousPostsController.js
+++ controllers/AnonymousPostsController.js
@@ -5,34 +5,56 @@ var config = require('../config.js');
 
 //create a post
 exports.create = function(req, res) {
-  var anonymouspost = new AnonymousPost({
-    message: req.body.message,
-    author: req.body.author,
-	  handle: req.body.handle,
-    location: req.body.location
-  });  
-  
-  anonymouspost.save(function(err) {
-    if (err){
-      res.send(err);
-  	} else {
-      res.header('Status', 201);
-      res.header('Location',req.hostname+'/anonymousposts/'+anonymouspost.id);
-      res.json({ 
-          createdAt: anonymouspost.createdAt,
-          objectId: anonymouspost.id
+  if (!req.user || !req.body.message) {
+    res.status = 400;
+    res.json({
+      status: 400,
+      message: "Bad request"
+    })
+  } else {
+    var anonymouspost = new AnonymousPost({
+      message: req.body.message,
+      author: req.user.id,
+      handle: req.body.handle,
+      location: req.body.location
+    });
+
+    User.findOne({
+      username: req.user.username
+    }, function(err, user) {
+      if (err || !user) {
+        res.status = 500;
+        res.json({
+          status: 500,
+          message: "Oops! Something went Wrong"
+        })
+      } else {
+        anonymouspost.institution = user.institution
+        anonymouspost.save(function(err) {
+          if (err) {
+            res.send(err);
+          } else {
+            res.header('Status', 201);
+            res.header('Location', req.hostname + '/anonymousposts/' +
+              anonymouspost.id);
+            res.json({
+              createdAt: anonymouspost.createdAt,
+              objectId: anonymouspost.id
+            });
+          }
         });
-   }
-  });
+      }
+    })
+  }
 };
 
 //get latest posts
 exports.getLatest = function(req, res) {
-	AnonymousPost.find().sort('-date').limit(10).exec(function(err, users) {
-    if (err){
-       res.send(err);
-    }else{
-       res.json(users);
+  AnonymousPost.find().sort('-date').limit(10).exec(function(err, users) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(users);
     }
   });
 };
@@ -41,21 +63,21 @@ exports.getLatest = function(req, res) {
 exports.getHottest = function(req, res) {
   //(net upvotes) / (time passed). and sort
   AnonymousPost.find(function(err, users) {
-    if (err){
-       res.send(err);
-    }else{
-       res.json(users);
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(users);
     }
   });
 };
 
 //get a single post with comments list
 exports.getOne = function(req, res) {
-	AnonymousPost.findById(req.params.id, function(err, anonymouspost) {
-    if (err){
+  AnonymousPost.findById(req.params.id, function(err, anonymouspost) {
+    if (err) {
       res.send(err);
-    }else{
-      res.json(anonymouspost);      
+    } else {
+      res.json(anonymouspost);
     }
   });
 };
@@ -63,11 +85,13 @@ exports.getOne = function(req, res) {
 //to upvote a post
 exports.upvote = function(req, res) {
   AnonymousPost.findById(req.params.id, function(err, anonymouspost) {
-    if (err){
+    if (err) {
       res.send(err);
-    }else{
+    } else {
       anonymouspost.upvote(user);
-      res.json({vote:'true'});      
+      res.json({
+        vote: 'true'
+      });
     }
   });
 };
@@ -75,10 +99,10 @@ exports.upvote = function(req, res) {
 //downvote a post
 exports.downvote = function(req, res) {
   AnonymousPost.find(req.params.id, function(err, user) {
-    if (err){
+    if (err) {
       res.send(err);
-    }else{
-      res.json(user);      
+    } else {
+      res.json(user);
     }
   });
 };
@@ -89,39 +113,42 @@ exports.createComment = function(req, res) {
     message: req.body.message,
     author: req.body.author,
     location: req.body.location
-  });  
+  });
   comment.save(function(err) {
-    if (err){
+    if (err) {
       res.send(err);
-  	} else {
+    } else {
       res.header('Status', 201);
-      res.header('Location',req.hostname+'/anonymousposts/'+anonymouspost.id);
-      res.json({ 
-          createdAt: comment.createdAt,
-          objectId: comment.id
-        });
-   }
+      res.header('Location', req.hostname + '/anonymousposts/' +
+        anonymouspost.id);
+      res.json({
+        createdAt: comment.createdAt,
+        objectId: comment.id
+      });
+    }
   });
 };
 
 //to upvote a comment
 exports.upvoteComment = function(req, res) {
-  
+
 };
 
 //to downvote a comment
 exports.downvoteComment = function(req, res) {
-  
+
 };
 
 
 //delete a comment
 exports.deleteComment = function(req, res) {
   AnonymousPostComment.findByIdAndRemove(req.params.id, function(err) {
-    if (err){
+    if (err) {
       res.send(err);
-    }else {
-      res.json({ message: 'comment deleted' }); 
+    } else {
+      res.json({
+        message: 'comment deleted'
+      });
     }
   });
 };
@@ -129,20 +156,22 @@ exports.deleteComment = function(req, res) {
 //delete a post
 exports.delete = function(req, res) {
   AnonymousPost.findByIdAndRemove(req.params.id, function(err) {
-    if (err){
+    if (err) {
       res.send(err);
-    }else {
-      res.json({ message: 'post deleted' }); 
+    } else {
+      res.json({
+        message: 'post deleted'
+      });
     }
   });
 };
 
 //report a post
 exports.reportPost = function(req, res) {
-	
+
 };
 
 //report a comment
 exports.reportComment = function(req, res) {
-	
+
 };
