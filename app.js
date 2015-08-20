ed89c81a2cc4fe3e897551c43a9766ec2545ea3b
Balamurali Pandranki <balamurali@live.com>
2015-08-20 15:19:05 +0530

    middleware called via individual routes

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ app.all('/*', function(req, res, next) {
 // Only the requests that start with /api/* will be checked for the token.
 // Any URL's that do not follow the below pattern should be avoided unless you
 // are sure that authentication is not needed
-app.all('/api/*', require('./middlewares/validateRequest').validateRequest);
+//app.all('/api/*', require('./middlewares/validateRequest').validateRequest);
 
-app.use('/api/v1', routes);
+app.use('/api/1', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes.js
+++ routes.js
@@ -11,15 +11,16 @@ var notifications = require('./controllers/NotificationsController.js');
 var appsconfig = require('./lib/appsconfig.js');
 var events = require('./controllers/EventsController.js');
 var offers = require('./controllers/OffersController.js');
+var validateRequest = require('./middlewares/validateRequest').validateRequest;
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({
-    status: 200,
-    message: 'this is end of your limits dude!!!',
-    uptime: process.uptime()
-  });
+router.get('/', function (req, res, next) {
+    res.json({
+        status: 200,
+        message: 'this is end of your limits dude!!!',
+        uptime: process.uptime()
+    });
 });
 
 
@@ -27,84 +28,74 @@ router.get('/', function(req, res, next) {
  * User routes
  */
 router.post('/users/login', users.login);
-router.post('/users/logout', users.logout);
+router.post('/users/logout', validateRequest, users.logout);
 router.post('/users/reset_password', users.resetPassword);
 router.post('/users', users.create);
-router.get('/users/me', users.authenticatedUser);
-router.get('/users/settings', users.getSettings);
-router.put('/users/settings', users.updateSettings);
-router.get('/users/:username', users.getOne);
-router.put('/users/:username', users.update);
-router.get('/users', users.getAll);
-/*
- * Installations routes
- */
-router.post('/installations', installations.create);
-router.get('/installations', installations.getAll);
-router.get('/installations/:id', installations.getOne);
-router.put('/installations/:id', installations.update);
-router.delete('/installations/:id', installations.delete);
-
+router.get('/users/me', validateRequest, users.authenticatedUser);
+router.get('/users/settings', validateRequest, users.getSettings);
+router.put('/users/settings', validateRequest, users.updateSettings);
+router.get('/users/:username', validateRequest, users.getOne);
+router.put('/users/:username', validateRequest, users.update);
+router.get('/users', validateRequest, users.getAll);
 
 /*
  * Posts routes
  */
-router.post('/feed', posts.create);
-router.get('/feed', newsfeed.getFeed);
-router.get('/feed/:id', posts.getOne);
-router.get('/feed/:id/likes', posts.getLikes);
-router.post('/feed/:id/like', posts.likePost); //represented with 1,0,-1
-router.get('/feed/:id/comments', posts.getAllcomments);
-router.post('/feed/:id/comments', posts.createComment);
-router.get('/feed/:id/comments/:id', posts.getComment);
-router.delete('/feed/:id/comments/:id', posts.deleteComment);
-router.put('/feed/:id', posts.update);
-router.delete('/feed/:id', posts.delete);
+router.post('/feed', validateRequest, posts.create);
+router.get('/feed', validateRequest, newsfeed.getFeed);
+router.get('/feed/:id', validateRequest, posts.getOne);
+router.get('/feed/:id/likes', validateRequest, posts.getLikes);
+router.post('/feed/:id/like', validateRequest, posts.likePost); //represented with 1,0,-1
+router.get('/feed/:id/comments', validateRequest, posts.getAllcomments);
+router.post('/feed/:id/comments', validateRequest, posts.createComment);
+router.get('/feed/:id/comments/:id', validateRequest, posts.getComment);
+router.delete('/feed/:id/comments/:id', validateRequest, posts.deleteComment);
+router.put('/feed/:id', validateRequest, posts.update);
+router.delete('/feed/:id', validateRequest, posts.delete);
 
 
 /*
  * Installations routes
  */
-router.post('/installations', installations.create);
-router.get('/installations', installations.getAll);
-router.get('/installations/:id', installations.getOne);
-router.put('/installations/:id', installations.update);
-router.delete('/installations/:id', installations.delete);
+router.post('/installations', validateRequest, installations.create);
+router.get('/installations', validateRequest, installations.getAll);
+router.get('/installations/:id', validateRequest, installations.getOne);
+router.put('/installations/:id', validateRequest, installations.update);
+router.delete('/installations/:id', validateRequest, installations.delete);
 
 
 /*
  * Notification routes
  */
 //router.post('/notifications', notifications.create);
-router.get('/notifications', notifications.getAll);
-router.get('/notifications/:id', notifications.getOne);
-router.post('/notifications/:id', notifications.update); //mark as read
+router.get('/notifications', validateRequest, notifications.getAll);
+router.get('/notifications/:id', validateRequest, notifications.getOne);
+router.post('/notifications/:id', validateRequest, notifications.update); //mark as read
 
-//router.put('/notifications/:id', notifications.update);
-//router.delete('/notifications/:id', notifications.delete);
+//router.put('/notifications/:id',validateRequest, notifications.update);
+//router.delete('/notifications/:id',validateRequest, notifications.delete);
 
 /*
  * Anonymous Posts Routes
  */
-router.get('/anonymousposts', anonymousposts.getLatest); //latest posts
-router.post('/anonymousposts', anonymousposts.create); //latest posts
-router.get('/anonymousposts/hot', anonymousposts.getHottest); //latest posts
-router.get('/anonymousposts/:id', anonymousposts.getOne);
-router.get('/anonymousposts/:id', anonymousposts.reportPost);
-router.get('/anonymousposts/:id/upvote', anonymousposts.upvote); // represented with 1,0,-1
-router.get('/anonymousposts/:id/downvote', anonymousposts.downvote); // represented with 1,0,-1
-router.post('/anonymousposts/:id/comments', anonymousposts.createComment);
-router.get('/anonymousposts/:postid/comments/:commentid/upvote', anonymousposts
-  .upvoteComment);
-router.get('/anonymousposts/:postid/comments/:commentid/downvote',
-  anonymousposts.downvoteComment);
-router.delete('/anonymousposts/:postid/comments/:commentid', anonymousposts.deleteComment);
-router.post('/anonymousposts/:postid/comments/:commentid', anonymousposts.reportComment);
-router.delete('/anonymousposts/:id', anonymousposts.delete);
+router.get('/anonymousposts', validateRequest, anonymousposts.getLatest); //latest posts
+router.post('/anonymousposts', validateRequest, anonymousposts.create); //latest posts
+router.get('/anonymousposts/hot', validateRequest, anonymousposts.getHottest); //latest posts
+router.get('/anonymousposts/:id', validateRequest, anonymousposts.getOne);
+router.get('/anonymousposts/:id', validateRequest, anonymousposts.reportPost);
+router.get('/anonymousposts/:id/upvote', validateRequest, anonymousposts.upvote); // represented with 1,0,-1
+router.get('/anonymousposts/:id/downvote', validateRequest, anonymousposts.downvote); // represented with 1,0,-1
+router.post('/anonymousposts/:id/comments', validateRequest, anonymousposts.createComment);
+router.get('/anonymousposts/:postid/comments/:commentid/upvote', validateRequest, anonymousposts
+    .upvoteComment);
+router.get('/anonymousposts/:postid/comments/:commentid/downvote', validateRequest, anonymousposts.downvoteComment);
+router.delete('/anonymousposts/:postid/comments/:commentid', validateRequest, anonymousposts.deleteComment);
+router.post('/anonymousposts/:postid/comments/:commentid', validateRequest, anonymousposts.reportComment);
+router.delete('/anonymousposts/:id', validateRequest, anonymousposts.delete);
 
 //Files routes
-router.post('/files/:name', files.upload);
-router.delete('/files/:name', files.delete);
+router.post('/files/:name', validateRequest, files.upload);
+router.delete('/files/:name', validateRequest, files.delete);
 
 //App Config routes
 router.get('/appconfig', appsconfig.getCurrentConfig);
